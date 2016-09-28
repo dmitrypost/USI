2f8919716c74acd466a82a3d6610430df760637d
Dmitry Post <dpost@eagles.usi.edu>
2016-09-28 00:06:16 -0500

    pear library added

---
+++ Libraries/go-pear.php
@@ -0,0 +1,16 @@
+Pear installation instructions:
+
+Windows and Wamp:
+browse to wamp installation folder
+browse to subdirectory bin/php/
+browse to subdirectory of php version being used
+
+execute phar.phar.bat
+
+after that is installed
+run the command below
+pear install Mail-1.3.0
+
+it will prompt for local or system type local
+make sure to set parameter 13 to the php location
+
