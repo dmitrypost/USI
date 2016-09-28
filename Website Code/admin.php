4045ae026eb46715f6bf55bd2497473e5dde9000
Dmitry Post <dpost@eagles.usi.edu>
2016-09-27 22:03:31 -0500

    .

---
+++ Website Code/admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin </title>
+        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+        <meta http-equiv="content-type" content="text/html; charset-utf-8"/>
+        <!-- Javascript source files initiation area -->
+
+
+    </head>
+    <body>
+        <div id="">
+        <?php 
+        
+        ?>
+
+    </body>
+</html>
