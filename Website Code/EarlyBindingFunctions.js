8cece796501bd472f39dbe72bc8bc05194f76ddd
dmitrypost <dpost@eagles.usi.edu>
2016-10-03 11:38:25 -0500

    Added Navicationing
    Added php files for navicationing and ability to navigate with ajax and
    not reload page every time going to another area

--- Website Code/EarlyBindingFunctions.js
+++ Website Code/EarlyBindingFunctions.js
@@ -26,6 +26,7 @@ function replaceHtml(element,html) {
 //function returns either Login & Register Or Loggedin Users Name & Manage buttons
 function getRegisterLoginUserLinks()
 {
+    var request;
 	console.log('called');
 	serializedData = "" ;
 	request = $.ajax({
@@ -36,7 +37,11 @@ function getRegisterLoginUserLinks()
 	// Callback handler that will be called on success
 	request.done(function (response, textStatus, jqXHR){
 		console.log('request done');
-		
+		var res = response.split("|");
+		console.log(res[0]);
+		console.log(res[1]);
+		replaceHtml('login/user', res[0]);              //replaces the link with either the logged in user link or login link
+		replaceHtml('register/projects', res[1]);       //replaces the link with either the projects link or register link
 	});
 	// Callback handler that will be called on failure
 	request.fail(function (jqXHR, textStatus, errorThrown){
@@ -44,3 +49,44 @@ function getRegisterLoginUserLinks()
 	});	
 	return false;	
 }
+
+//function that adds to browsing history 
+function AddToHistory(newUrl)
+{
+    //not implamented
+    return false;
+}
+
+
+//function makes a call to php to get the logged in user's profile
+function showProfile(uid)
+{
+    var request = $.ajax({url:"Body.php",type: "post", data: "uid=" + uid })
+    request.done(function (response, textStatus, jqXHR) {
+        console.log('request done');
+        replaceHtml('BodyPanel', response);              //replaces the link with either the logged in user link or login link
+    });
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+        console.error("The following error occurred: " + textStatus, errorThrown);
+    });
+    return false;
+}
+
+//function that calls php body to get relative information based on url
+function getBody()
+{
+    //window.location.search is only the ?= ... part
+    var request = $.ajax({ rl: "Body.php", type: "post", data: window.location.search })
+    request.done(function (response, textStatus, jqXHR) {
+        console.log('request done');
+        replaceHtml('BodyPanel', response);              //replaces the link with either the logged in user link or login link
+    });
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+        console.error("The following error occurred: " + textStatus, errorThrown);
+    });
+    return false;
+}
+
+
