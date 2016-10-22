18f74c56c9d6a26824af596defd61c86b3ef32c6
Dmitry Post <Dpost@eagles.usi.edu>
2016-10-21 19:14:25 -0500

    +foreign key to tblCollege

--- SQL/database.sql
+++ SQL/database.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tblUser (usr_id INT NOT NULL AUTO_INCREMENT, usr_email TINYTEXT,usr_password TINYTEXT, usr_fname TINYTEXT, usr_lname TINYTEXT, usr_picture MEDIUMTEXT, usr_admin TINYINT, usr_mgr_id INT, usr_graduate TINYINT, usr_onetimepass TINYINT, usr_pageview INT, usr_linkedin TINYTEXT, usr_phone TINYTEXT, PRIMARY KEY (usr_id));
 CREATE TABLE tblRole (rol_usr_id INT NOT NULL AUTO_INCREMENT, rol_pjt_id INT, rol_name TINYTEXT, CONSTRAINT rol_id PRIMARY KEY (rol_usr_id,rol_pjt_id));
-CREATE TABLE tblMajor (mgr_id INT NOT NULL AUTO_INCREMENT, mgr_name TINYTEXT, mgr_key_id INT, PRIMARY KEY (mgr_id));
+CREATE TABLE tblMajor (mgr_id INT NOT NULL AUTO_INCREMENT, mgr_name TINYTEXT, mgr_key_id INT, mgr_clg_id INT, PRIMARY KEY (mgr_id));
 CREATE TABLE tblKeyword (kwd_id INT NOT NULL AUTO_INCREMENT, kwd_name TINYTEXT, PRIMARY KEY (kwd_id));
 CREATE TABLE tblKeywordAssociation (key_id INT, key_kwd_id INT, PRIMARY KEY (key_id,key_kwd_id));
 CREATE TABLE tblProject (pjt_id INT NOT NULL AUTO_INCREMENT, pjt_name TINYTEXT, pjt_body LONGTEXT, pjt_description TINYTEXT, pjt_key_id INT, pjt_clg_id INT, pjt_pageview INT, pjt_year YEAR, PRIMARY KEY (pjt_id));
@@ -20,3 +20,4 @@ ALTER TABLE tblFile ADD FOREIGN KEY (fle_pjt_id) REFERENCES tblProject(pjt_id);
 ALTER TABLE tblProjectHistory ADD FOREIGN KEY (pjh_usr_id) REFERENCES tblUser(usr_id);
 ALTER TABLE tblProjectHistory ADD FOREIGN KEY (pjh_pjt_id) REFERENCES tblProject(pjt_id);
 ALTER TABLE tblSession ADD FOREIGN KEY (ses_usr_id) REFERENCES tblUser(usr_id);
+ALTER TABLE tblMajor ADD FOREIGN KEY (mgr_clg_id) REFERENCES tblCollege(clg_id);
