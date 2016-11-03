5262f8e487fd17a37d56e7fbd95ced5ab9ac3445
dgtang <dana.g.k.tang@gmail.com>
2016-11-03 18:37:24 -0500

    deleted side panel text box
    commented the white div that said "side panel content here"

--- Website Code/PHP/Default.php
+++ Website Code/PHP/Default.php
@@ -2,9 +2,35 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>Project Repository Home</title>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://www.usi.edu/f4/css/foundation.css">
+    <link rel="stylesheet" type="text/css" href="http://www.usi.edu/css/departments.css">
+    <link rel="stylesheet" type="text/css" href="CSS/foundation2.css">
+	<script src="http://www.usi.edu/f4/js/foundation.min.js"></script>
+	<script src="JS/EarlyBindingFunctions.js"></script>    
+    <script src="JS/ProcessRegister.js"></script>
 </head>
 
 <body>
+<h1 id ="homePage_title">Project Repository</h1>
+<p>Welcome to University of Southern Indiana Project Repository.  Here, you can find projects completed by students inside and outside of the classroom.</p>
+<div id ="homeHeaderImg">
+	<img src="" alt="">
+    <h5 class="descrTitles">Our 2016 Alberta Energy Challenge Team</h5>
+    <p>We cou't be more proud of ourr 2016 Alberta Energy Challenge team and faculty advisors Jeanette Maier-Lytle and Dr. Brandon Field! Check 			out the impressive statistics of the case teams that have represented USI Romain College nationally and internationally. </p>
+</div>
+<div id = "homeImg1_Row2">
+	<img src="" alt="">
+    <h5 class="descrTitles">Three Earn Toastmasters Designations</h5>
+     <p>Three USI Speaking EAgles club officers earn Toastmasters certifications.</p>
+</div>
+<div id = "homeImg2_Row2">
+	<img src="" alt="">
+    <h5 class="descrTitles">Title Here</h5>
+     <p>Description of cool senior project here!</p>
+</div>
 </body>
 </html>
