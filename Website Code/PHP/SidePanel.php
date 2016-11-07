9aac8ecf4c28545c69a84d3619cc8e681d4efaf2
Dmitry Post <Dpost@eagles.usi.edu>
2016-11-07 17:24:36 -0600

    updated to change back to sidepanel content loading...

--- Website Code/PHP/SidePanel.php
+++ Website Code/PHP/SidePanel.php
@@ -1,7 +1,4 @@
 <?php
-/*
-	
-*/
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') 
     { 
 		$defaultPage = true;
@@ -10,39 +7,18 @@
 		  $defaultPage = false;
 		  switch ($key)
 			{
-				case 'euid':
-					echo "edit profile";	
-					include 'EditProfile.php';		
-					break;	
-				case 'uid':
-					//echo "profile";
-					include 'Profile.php';
-					break;
-				case 'epid':
-					echo "edit project";
-					include 'EditProject.php';
-					break;
+
 				case 'pid';
 					echo "project";
 					include 'Project.php';
 					break;
-				case 's';
-					echo "search";
-					include 'Search.php';
-					break;
-				case 'pjs':
-					echo "user projects page";
-					include 'Projects.php';
-					break;	
-				case 'logout':
-					include 'Logout.php';
-					break;		
+
 			}
 			
 		}
 		if ($defaultPage)
 		{
-			echo "default landing page";
+			include "SidePanel/Deafault.php";
 		}
 	}
 ?>
