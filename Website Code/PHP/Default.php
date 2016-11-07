f9ded161dadf5fd625f1cc2a9f257dec16cd5d27
dgtang <dana.g.k.tang@gmail.com>
2016-11-07 14:37:54 -0600

    added html and css

--- Website Code/PHP/Default.php
+++ Website Code/PHP/Default.php
@@ -12,25 +12,75 @@
 	<script src="http://www.usi.edu/f4/js/foundation.min.js"></script>
 	<script src="JS/EarlyBindingFunctions.js"></script>    
     <script src="JS/ProcessRegister.js"></script>
+    <style>
+	.descrTitles, #homePage_title{
+		color:red;
+	}
+	p{
+		font-size: 16px;
+		margin:0px;
+	}
+	#homePage_title{
+		margin:0px;
+	}
+	#homeHeaderImg{
+		
+	}
+	#leftpics{
+		float:left;
+		width:50%;
+	}
+	#rightpics{
+		float:left;
+		width:50%;
+	}
+	#smallpics{
+		float:left;
+		clear:both;
+		/*border-style:solid;*/
+		
+	}
+	</style>
 </head>
-
 <body>
-<h1 id ="homePage_title">Project Repository</h1>
-<p>Welcome to University of Southern Indiana Project Repository.  Here, you can find projects completed by students inside and outside of the classroom.</p>
-<div id ="homeHeaderImg">
-	<img src="" alt="">
-    <h5 class="descrTitles">Our 2016 Alberta Energy Challenge Team</h5>
-    <p>We cou't be more proud of ourr 2016 Alberta Energy Challenge team and faculty advisors Jeanette Maier-Lytle and Dr. Brandon Field! Check 			out the impressive statistics of the case teams that have represented USI Romain College nationally and internationally. </p>
-</div>
-<div id = "homeImg1_Row2">
-	<img src="" alt="">
-    <h5 class="descrTitles">Three Earn Toastmasters Designations</h5>
-     <p>Three USI Speaking EAgles club officers earn Toastmasters certifications.</p>
-</div>
-<div id = "homeImg2_Row2">
-	<img src="" alt="">
-    <h5 class="descrTitles">Title Here</h5>
-     <p>Description of cool senior project here!</p>
+<div>
+    <h1 id ="homePage_title">Project Repository</h1>
+    <p>Welcome to University of Southern Indiana Project Repository.  Here, you can find projects completed by students inside and outside of the classroom.</p>
+     <!---------------------------------------------------------->
+    <div id ="homeHeaderImg">
+        <img src="AlbertaEnergyChallenge.PNG" alt="Energy Challenge Team">
+        <h5 class="descrTitles">Our 2016 Alberta Energy Challenge Team</h5>
+        <p>We cou't be more proud of ourr 2016 Alberta Energy Challenge team and faculty advisors Jeanette Maier-Lytle and Dr. Brandon Field! Check out the impressive statistics of the case teams that have represented USI Romain College nationally and internationally. </p>
+    </div>
+    
+    <div id ="smallpics">
+    <!---------------------------------------------------------->
+        <div id = "leftpics">
+            <div id = "homeImg1_Row2">
+                <img src="" alt="picture">
+                <h5 class="descrTitles">Three Earn Toastmasters Designations</h5>
+                 <p>Three USI Speaking EAgles club officers earn Toastmasters certifications.</p>
+            </div>
+            <div id = "homeImg2_Row3">
+                <img src="" alt="picture">
+                <h5 class="descrTitles">Title Here</h5>
+                 <p>Description of cool senior project here!</p>
+            </div>
+        </div>
+         <!---------------------------------------------------------->
+        <div id ="rightpics">
+            <div id = "homeImg1_Row2">
+                <img src="" alt="picture">
+                <h5 class="descrTitles">Three Earn Toastmasters Designations</h5>
+                 <p>Three USI Speaking EAgles club officers earn Toastmasters certifications.</p>
+            </div>
+            <div id = "homeImg2_Row3">
+                <img src="" alt="picture">
+                <h5 class="descrTitles">Title Here</h5>
+                 <p>Description of cool senior project here!</p>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
