15b4b9cae641e28f567a2ddf5791c7c976fef4b4
cfoster5 <Corey@Coreys-MacBook-Pro.local>
2016-11-07 14:30:48 -0600

    Changes made to Edit Profile Page
    Added text boxes for a user to change their legal name, phone number,
    and email address. Added a dropdown menu from majors listed in the
    database. Need to check with Dmitry to get the name changes and contact
    info changes to reflect in the database.

--- Website Code/PHP/EditProfile.php
+++ Website Code/PHP/EditProfile.php
@@ -15,5 +15,32 @@
             <h1 id="page-name">Edit Profile Page</h1>
         </div>
     </div>
+
+    <h5 class="descrTitles">Change Legal Name</h5>
+    <form action="demo_form.asp">
+    First name: <input type="text" name="FirstName" value="John" maxlength="20" size="20"><br>
+    Last name: <input type="text" name="LastName" value="Doe" maxlength="20" size="20">
+    </form>
+
+    <h5 class="descrTitles">Change Major</h5>
+    <select name="slt_major" id="slt_major">
+      <?php
+      include_once 'Database.php';
+      $con = open();
+      $query = "SELECT mgr_clg_id, mgr_name FROM tblMajor";
+      if ($result = mysqli_query($con, $query)){if (mysqli_num_rows($result) > 0 ) { while($row = mysqli_fetch_assoc($result)){
+        echo "<option value=".$row['mgr_clg_id'].">".$row['mgr_name']."</option>";
+      }
+      } else { /*no results found */ }
+      } else { echo 'error';}
+      mysqli_close($con);
+    ?>
+    </select>
+
+    <h5 class="descrTitles">Change Contact Information</h5>
+    Phone Number: <input type="text" name="PhoneNumber" value="812-123-4567" maxlength="10" size="10"><br>
+    Email: <input type="text" name="Email" value="account@service.com" maxlength="30" size="30">
+    <br>
+
 </body>
 </html>
