55caa1438fd44d76c5fb2a9ab0c173330e19e8e0
dgtang <dana.g.k.tang@gmail.com>
2016-11-17 18:18:13 -0600

    html new page

---
+++ Website Code/PHP/EditPassword.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Edit Password</title>
+<style>
+#passwdChange{
+	
+	list-style-type: none;
+}
+</style>
+</head>
+
+<body>
+<ul id ="passwdChange">
+	<li>
+    <h1>Edit Password</h1>
+    </li>
+	Current Password:
+    <li>
+     <input type="text"  value="" maxlength="50";>
+     </li>
+     <br>
+     New Password:
+     <li>
+     <input type="text"  value="" maxlength="50";>
+     </li>
+      <br>
+      Repeat New Password:
+      <li>
+     <input type="text"  value="" maxlength="50";>
+     </li>
+      <br>
+      <li>
+      <button class="accordianbtns">Submit</button>
+      </li>
+  </ul>
+
+</body>
+</html>
