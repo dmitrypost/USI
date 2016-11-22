e3ea620c1c0b24ca08d7b5f3817fc696dd9652fa
dgtang <dana.g.k.tang@gmail.com>
2016-11-22 13:13:19 -0600

    added css

---
+++ Website Code/Templates/approvals.dwt.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Project Approval</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link rel="stylesheet" type="text/css" href="/../foundation2.css">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="/resources/demos/style.css">
+<style>
+	.projApprovalBox li{
+	margin-bottom: 5px;
+	list-style-type: none;
+	}
+</style>
+</head>
+
+<body>
+<div id="accordion">
+  <h3>USI Project Repository</h3><!--------------------------------------------------------->
+  <div>
+   <ul class="projApprovalBox">
+    <li><h5>Edit submitted by student name:</h5></li>
+    <li><input type="text"  value="Change in body" maxlength="50";></li>
+   <li><input type="text"  value="change in description" maxlength="50";></li>
+   <li><input type="text"  value="change in title" maxlength="50";></li>
+   <li><input type="text"  value="pending removal of participants" maxlength="50";></li>
+	<li><button class="accordianbtns">Approve</button></li>
+	<li><button class="accordianbtns">Deny</button></li>
+	</ul>
+  </div>
+  <h3>Project Name Here</h3><!---------------------------------------------------------------->
+  <div>
+    <ul class="projApprovalBox">
+    <li><h5>Edit submitted by student name:</h5></li>
+    <li><input type="text"  value="Change in body" maxlength="50";></li>
+   <li><input type="text"  value="change in description" maxlength="50";></li>
+   <li><input type="text"  value="change in title" maxlength="50";></li>
+   <li><input type="text"  value="pending removal of participants" maxlength="50";></li>
+	<li><button class="accordianbtns">Approve</button></li>
+	<li><button class="accordianbtns">Deny</button></li>
+	</ul>
+  </div>
+</div>
+</section>
+</body>
+</html>
