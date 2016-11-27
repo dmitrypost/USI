e2f5a628e0e5b6fd31e1bc824f2ffccf884f9ec9
Dmitry Post <Dpost@eagles.usi.edu>
2016-11-27 05:25:32 -0600

    Removed check for 'Action' as a passed variable

--- Website Code/PHP/Projects.php
+++ Website Code/PHP/Projects.php
@@ -2,11 +2,11 @@
 	include_once 'Functions.php'; //include_once 'Database.php'; //database.php is already included within the functions.php
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') 
 	{
-		if (isset($_POST['Action']) AND isset($_POST['value']))
+		if (isset($_POST['value']))
 		{
 			include_once 'Functions.php';
 			$con = Open();
-			$Action  = mysqli_real_escape_string($con,trim(strip_tags($_POST['Action']))); 
+			//$Action  = mysqli_real_escape_string($con,trim(strip_tags($_POST['Action']))); 
 			$Value  = mysqli_real_escape_string($con,trim(strip_tags($_POST['value']))); 
 			PageTitle("Projects");
 			if (isLoggedIn())
@@ -14,9 +14,7 @@
 				$uid = getUID();
 				if ($Value == "Owned")
 				{
-					
-					
-						$query = "SELECT rol_name, pjt_name, pjt_description, mgr_name, usr_id , pjt_year, pjt_id FROM tbluser INNER JOIN tblrole ON tbluser.usr_id = tblrole.rol_usr_id INNER JOIN tblproject ON tblrole.rol_pjt_id = tblproject.pjt_id INNER JOIN tblMajor ON tblproject.pjt_mgr_id = tblMajor.mgr_id WHERE usr_id = ". $uid;
+						$query = "SELECT rol_name, pjt_name, pjt_description, mgr_name, usr_id , pjt_year, pjt_id FROM tbluser INNER JOIN tblrole ON tbluser.usr_id = tblrole.rol_usr_id INNER JOIN tblproject ON tblrole.rol_pjt_id = tblproject.pjt_id INNER JOIN tblMajor ON tblproject.pjt_mgr_id = tblMajor.mgr_id WHERE usr_id =$uid";
 						if ($result = mysqli_query($con, $query)){if (mysqli_num_rows($result) > 0){ while($row = mysqli_fetch_assoc( $result)) {
 							FormattedProjectPreview($row['pjt_name'],$row['pjt_description'],$row['pjt_year'],$row['pjt_id'],true);
 						}}}	
@@ -25,7 +23,7 @@
 				elseif($Value == "All")
 				{
 					
-						$query = "SELECT rol_name, pjt_name, pjt_description, pjt_year, pjt_id FROM tblProject LEFT JOIN tblrole ON tblrole.rol_pjt_id = tblproject.pjt_id WHERE rol_usr_id = ". $uid;
+						$query = "SELECT rol_name, pjt_name, pjt_description, pjt_year, pjt_id FROM tblProject LEFT JOIN tblrole ON tblrole.rol_pjt_id = tblproject.pjt_id WHERE rol_usr_id = $uid";
 						if ($result = mysqli_query($con, $query)){if (mysqli_num_rows($result) > 0){ while($row = mysqli_fetch_assoc( $result)) {
 							
 							FormattedProjectPreview($row['pjt_name'],$row['pjt_description'],$row['pjt_year'],$row['pjt_id'],strlen($row['rol_name'])>0);
