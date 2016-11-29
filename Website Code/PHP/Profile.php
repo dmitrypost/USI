226354c812dbb5bdb52b5f85315ba77c5a2fa887
cfoster5 <Corey@Coreys-MacBook-Pro.local>
2016-11-28 20:53:12 -0600

    CSS Changes

--- Website Code/PHP/Profile.php
+++ Website Code/PHP/Profile.php
@@ -25,13 +25,13 @@
 		//
 	echo "
 	<div class='stafflist'>
-		<div class='row'>	
+		<div class='row'>
 			<div class='small-2 large-2 columns'>
 				<img src='".$profilePic."' alt='".$NoProfilePictureImage."'>
 			</div>
 			<div class='small-12 large-5 columns info left'>
-				<h6>Major: ".$major."</h6>
-				".$grad." student
+				<h5>Major: ".$major."</h5>
+				Academic Status: ".$grad." student
 				<br>Contact: <a href='tel:".$row['usr_phone']."' title='call number'>".$row['usr_phone']."</a>
 				<br>Email: <a href='mailto:".$row['usr_email']."' >".$row['usr_email']."</a>
 				<br>LinkedIn: <a href='".$row['usr_linkedin']."' >".$row['usr_linkedin']."</a>
@@ -42,35 +42,35 @@
 	";
 	$query2 = "SELECT rol_name, pjt_name, pjt_description, mgr_name, usr_id , pjt_year, pjt_id FROM tbluser INNER JOIN tblrole ON tbluser.usr_id = tblrole.rol_usr_id INNER JOIN tblproject ON tblrole.rol_pjt_id = tblproject.pjt_id LEFT JOIN tblMajor ON tblproject.pjt_mgr_id = tblMajor.mgr_id WHERE usr_id = ". $uid;
 	if ($result2 = mysqli_query($con, $query2)){if (mysqli_num_rows($result2) > 0){
-		echo "<div class='center'>Projects ".$row['usr_fname']." participated in</div><br>";
+		echo "<div class='center'><h4>Projects ".$row['usr_fname']." has participated in:</h4></div><br>";
 		while($row2 = mysqli_fetch_assoc( $result2)) {
 		echo "
 		<div class='project-preview'>
-			<h5><a href='javascript:void(0)' onClick='showProject(".$row2['pjt_id'].")'>".$row2['pjt_name']."</a> - ".$row2['rol_name']."</h5> 
+			<h5><a href='javascript:void(0)' onClick='showProject(".$row2['pjt_id'].")'>".$row2['pjt_name']."</a> - ".$row2['rol_name']."</h5>
 			<br>".$row2['pjt_description']."
-			<br>".$row2['pjt_year']."
+			<br><br><h6>Year: ".$row2['pjt_year']."</h6>
 			<br>
 		</div>
 		";
 	}
 	} else {echo 'no projects found for this user';}
 	} else {echo 'error';}
-	
-	
-	
-	
+
+
+
+
 	echo "
 		</div>
 	</div>
 	<div class='view-count'>Profile views: ".$row['usr_pageview']."</div>
-	<div>URL:<a>localhost/?uid=".$row['usr_id']."</a></div>";
+	";
 	//increment pageview
 	QuickQuery("UPDATE tblUser SET usr_pageview = usr_pageview +1 WHERE usr_id=".$uid);
 	}
 	} else {echo 'no user found with that id';}
 	} else {echo 'error';}
-	
-	
+
+
 	mysqli_close($con);
 ?>
 </body>
