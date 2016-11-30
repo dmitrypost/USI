5e8a4f30bce261735da3ddf6a1d518b58bd5e463
Dmitry Post <Dpost@eagles.usi.edu>
2016-11-30 15:00:07 -0600

    Update Admin links

--- Website Code/PHP/SidePanel/AdminLinks.php
+++ Website Code/PHP/SidePanel/AdminLinks.php
@@ -3,7 +3,7 @@
 	if (isAdmin())
 	{
 		echo "
-			<button class='alink' href='javascript:void(0)' onClick='GoToPage(\"AlterCollegeMajor\")' title='Add, edit, or delete colleges and or majors'>Add/Edit/Delete Colleges & Majors</button><br>
+			<button class='alink' href='javascript:void(0)' onClick='GoToPage(\"AlterCollegeMajor\")' title='Alter colleges and or majors'>Alter Colleges & Majors</button><br>
             <button class='alink' href='javascript:void(0)' onClick='GoToPage(\"AlterHomePage\")' title='Edit home page'>Edit Home Page</button><br>
             <button class='alink' href='javascript:void(0)' onClick='GoToPage(\"RunSQL\")' title='Run SQL query upon database'>Run SQL</button><br>
 			<button class='alink' href='javascript:void(0)' onClick='GoToPage(\"ProjectApprovals\")' title='Approve pending changes to projects'>Project Approvals</button><br>
