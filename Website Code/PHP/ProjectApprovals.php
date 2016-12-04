ff417adfbf1932afac0b7b0d7bd4ad79f32e3ec7
cfoster <cfoster3204@gmail.com>
2016-12-03 19:24:10 -0600

    Testing Approvals page

--- Website Code/PHP/ProjectApprovals.php
+++ Website Code/PHP/ProjectApprovals.php
@@ -1,1 +1,21 @@
-
+<?php
+<select id='pjh_id'>";
+include_once 'Database.php';
+          $con = open();
+          $query = "SELECT pjh_id, pjh_approved FROM tblProjectHistory ";
+          if ($result = mysqli_query($con, $query)){if (mysqli_num_rows($result) > 0){
+          while($row = mysqli_fetch_assoc( $result)) {
+              if ($row['pjh_id'] == $ProjectMajorId)
+              {
+                echo "<option value=".$row['pjh_id']." selected>".$row['pjh_approved']."</option>";
+              }
+              else
+              {
+                echo "<option value=".$row['pjh_id'].">".$row['pjh_approved']."</option>";
+              }
+          }
+          } else { /*no results found*/ }
+          } else {echo 'error';}
+    mysqli_close($con); echo "
+</select>
+?>
